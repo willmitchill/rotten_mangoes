f887089cf0447c352e24b7232ee4c6050a179310
Will <wmitchel@hotmail.com>
2014-08-04 20:48:15 -0700

    added user model/tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805010216) do
+ActiveRecord::Schema.define(version: 20140805034752) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20140805010216) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
