6c9d483e111cfeeb1450939c953b13344c4ed54d
Will <wmitchel@hotmail.com>
2014-08-05 12:49:01 -0700

    added sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,16 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to movies_path
+      redirect_to movies_path, notice: "Welcome back, #{user.firstname}!"
     else
+      flash.now[:alert] = "Log in failed..."
       render :new
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_path, notice: "Adios!"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
